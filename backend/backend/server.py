d904b25fef5b369c99ecd69101d6e0f557799b6f
defneduruozturkk-tech <defne.duru.ozturkk@gmail.com>
2026-03-12 13:50:07 +0300

    start server for railway

--- backend/backend/server.py
+++ backend/backend/server.py
@@ -835,3 +835,9 @@ logger = logging.getLogger(__name__)
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+mport os
+import uvicorn
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
